925a10151c69e7e465403e1bfb8ba747ab88a5dc
Sydney <sfpugh@seas.upenn.edu>
2024-01-29 12:17:58 -0500

    fixed data path issue

--- Naturally_Adversarial_Datasets/data.py
+++ Naturally_Adversarial_Datasets/data.py
@@ -2,16 +2,16 @@ import os
 import numpy as np
 from itertools import product
 
-def load_data(dataset, data_path='../data'):
+def load_data(dataset, data_dir_path):
     L_train, L_test, y_test = [], [], []
 
     for g in ['train', 'dev', 'test']: 
-        if os.path.exists(os.path.join(data_path, '%s_L_%s.npy' % (dataset, g))):
-            L = np.load(os.path.join(data_path, '%s_L_%s.npy' % (dataset, g)))
+        if os.path.exists(os.path.join(data_dir_path, '%s_L_%s.npy' % (dataset, g))):
+            L = np.load(os.path.join(data_dir_path, '%s_L_%s.npy' % (dataset, g)))
             L_train.append(L)
 
-            if os.path.exists(os.path.join(data_path, '%s_y_%s.npy' % (dataset, g))):
-                y = np.load(os.path.join(data_path, '%s_y_%s.npy' % (dataset, g)))
+            if os.path.exists(os.path.join(data_dir_path, '%s_y_%s.npy' % (dataset, g))):
+                y = np.load(os.path.join(data_dir_path, '%s_y_%s.npy' % (dataset, g)))
                 L_test.append(L)
                 y_test.append(y)
 
