b7febadede90a7c09328d38213e48c6991f9ed16
Sydney <sfpugh@seas.upenn.edu>
2024-01-29 13:21:20 -0500

    fixed typo

--- Naturally_Adversarial_Datasets/majority_vote.py
+++ Naturally_Adversarial_Datasets/majority_vote.py
@@ -1,12 +1,14 @@
 import numpy as np
 from scipy.special import softmax
 
+ABSTAIN         = -1
+
 class MajorityVote:
     def __init__(self, cardinality):
         self.cardinality = cardinality
 
     def __get_logits__(self, l):
-        return np.bincount(l[l != self.abstain], minlength=self.cardinality)
+        return np.bincount(l[l != ABSTAIN], minlength=self.cardinality)
         
     def predict_proba(self, L, method='softmax'):
         logits = np.apply_along_axis(self.__get_logits__, 1, L)
