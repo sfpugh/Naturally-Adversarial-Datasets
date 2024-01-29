76a64e56834e10fea8a982579ca17aa0eb3cc184
Sydney <sfpugh@seas.upenn.edu>
2024-01-29 12:23:54 -0500

    comments added

--- Naturally_Adversarial_Datasets/data.py
+++ Naturally_Adversarial_Datasets/data.py
@@ -1,15 +1,27 @@
 import os
 import numpy as np
-from itertools import product
 
 def load_data(dataset, data_dir_path):
-    L_train, L_test, y_test = [], [], []
+    '''
+    Load the data.
 
+    args: 
+        dataset (str) : dataset name
+        data_dir_path (str) : path to data directory
+
+    return:
+        L_train (ndarray) : an [n,p] matrix with values in  {-1,0,1,…,k-1}
+        L_test (ndarray) : an [m,p] matrix with values in  {-1,0,1,…,k-1}
+        y_test (ndarray) : an m-dimensional array with values {0,1,…,k-1}
+    '''
+    L_train, L_test, y_test = [], [], []
+    
     for g in ['train', 'dev', 'test']: 
         if os.path.exists(os.path.join(data_dir_path, '%s_L_%s.npy' % (dataset, g))):
             L = np.load(os.path.join(data_dir_path, '%s_L_%s.npy' % (dataset, g)))
             L_train.append(L)
 
+            # if have ground truth then append to test data
             if os.path.exists(os.path.join(data_dir_path, '%s_y_%s.npy' % (dataset, g))):
                 y = np.load(os.path.join(data_dir_path, '%s_y_%s.npy' % (dataset, g)))
                 L_test.append(L)
